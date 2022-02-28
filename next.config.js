c43c18fa86cac4a712b12f813c0bd1d7c7c56e9a
wil <Workoutwwill@gmail.com>
2022-02-28 16:02:48 -0500

    deleted nextconfig

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdnjs.cloudflare.com/ajax/libs/kute.js/1.6.1/kute.min.js" integrity="sha512-DeLO4mdijNCQ3uOZDb1u+sUadgQUQnPr3zlPejMZBNbLi9rreaGwBjIJXYPU8WNJpRWb9LG6RpP6kdodXw6AGg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>' : '',
-}
